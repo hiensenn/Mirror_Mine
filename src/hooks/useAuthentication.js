894fe9fc2bc2a0670fcbb778cef58d2006d4d73f
Tryvia Sua Frota Mais Inteligente <hiensen.cdl@gmail.com>
2024-11-18 20:01:51 -0300

    creating Post page

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -13,8 +13,6 @@ import { useState, useEffect } from "react";
 export const useAuthentication = () => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(null);
-  
-  
 
   //cleanup
   //deal with memory leak
@@ -29,8 +27,6 @@ export const useAuthentication = () => {
   }
 
   // register
-
-
 
   //creating user
 
@@ -57,32 +53,62 @@ export const useAuthentication = () => {
       console.log(error.message);
       console.log(typeof error.message);
 
-
       let systemErrorMessage;
 
       if (error.message.includes("Password")) {
         systemErrorMessage = "A senha pecisa conter pelo menos 6 caracteres";
       } else if (error.message.includes("email-already")) {
-        systemErrorMessage = "E-mail já cadastrado"
-        
+        systemErrorMessage = "E-mail já cadastrado";
       } else {
         systemErrorMessage =
           "Ocorreu um erro, por favor tente novamente mais tarde";
       }
-      
+
       setLoading(false);
-      
+
       setError(systemErrorMessage);
-      
     }
-      
   };
 
   //Logout - sign out
   const logout = () => {
+    checkIfIsCancelled();
+    signOut(auth);
+  };
+
+  //login -signin
+
+  const login = async(data) => {
     checkIfIsCancelled()
-    signOut(auth)
+
+    setLoading(true)
+    setError(false)
+
+    try {
+
+      await signInWithEmailAndPassword(auth, data.email, data.password)
+      setLoading(false);
+
+    } catch (error) {
+
+      let systemErrorMessage;
+
+      if (error.message.includes("user-not-found")) {
+       
+        systemErrorMessage = "Usuário não encontrado"
+      }else if (error.message.includes("wrong-password")) {
+        systemErrorMessage = "E-mail ou senha incorreta";
+      } else{
+        systemErrorMessage = "Ocoreu um erro: verifique seus dados"
+      }
+
+      
+      setError(systemErrorMessage);
+      setLoading(false);
+    }
+
   }
+
 
   useEffect(() => {
     return () => setCancelled(true);
@@ -94,5 +120,6 @@ export const useAuthentication = () => {
     error,
     loading,
     logout,
+    login,
   };
 };
--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -1,11 +1,83 @@
-import styles from './CreatePost.module.css'
+import styles from "./CreatePost.module.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
 
 const CreatePost = () => {
-  return (
-    <div>
-        <h1>Create Post</h1>
-    </div>
-  )
-}
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState([]);
+  const [formError, setFormEror] = useState("");
 
-export default CreatePost
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className={styles.create_post}>
+      <h1>Criar Post</h1>
+      <p>Escreva sobre o que quiser e compartilhe </p>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Título</span>
+          <input
+            type="text"
+            name="title"
+            required
+            placeholder="pense em um titulo"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+        </label>
+
+        <label>
+          <span>URL da imagem</span>
+          <input
+            type="text"
+            name="image"
+            required
+            placeholder="insira uma imagem que represente seu post"
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
+          />
+        </label>
+
+        <label>
+          <span>Conteúdo</span>
+          <textarea
+            name="body"
+            required
+            placeholder="insira o conteúdo do posto"
+            onChange={(e) => setBody(e.target.value)}
+            value={body}
+          />
+        </label>
+
+        <label>
+          <span>Tags</span>
+          <textarea
+          type='text'
+            name="tags"
+            required
+            placeholder="insira as tags separadas por virgula "
+            onChange={(e) => setTags(e.target.value)}
+            value={tags}
+          />
+        </label>
+        <button type="submit" className="btn">Cadastrar</button>
+        {/* {!loading && <button type="submit" className="btn">Cadastrar</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
+
+        {error && <p className="error">{error}</p>} */}
+      </form>
+    </div>
+  );
+};
+
+export default CreatePost;

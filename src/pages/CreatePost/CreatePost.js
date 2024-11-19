8d285f60004518b48b49fd49e15668a65ef4c180
Tryvia Sua Frota Mais Inteligente <hiensen.cdl@gmail.com>
2024-11-19 10:38:00 -0300

    inserting data in firebase backend db

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -1,5 +1,7 @@
 import styles from "./CreatePost.module.css";
+
 import { useState } from "react";
+import { useInsertDocument } from "../../hooks/useInsertDocument";
 import { useNavigate } from "react-router-dom";
 import { useAuthValue } from "../../context/AuthContext";
 
@@ -8,73 +10,116 @@ const CreatePost = () => {
   const [image, setImage] = useState("");
   const [body, setBody] = useState("");
   const [tags, setTags] = useState([]);
-  const [formError, setFormEror] = useState("");
+  const [formError, setFormError] = useState("");
+
+  const { user } = useAuthValue();
+
+  const navigate = useNavigate();
+
+  const { insertDocument, response } = useInsertDocument("posts");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setFormError("");
+
+    // validate image
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL.");
+    }
+
+    // create tags array
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
+
+    // check values
+    if (!title || !image || !tags || !body) {
+      setFormError("Por favor, preencha todos os campos!");
+    }
+
+    console.log(tagsArray);
+
+    console.log({
+      title,
+      image,
+      body,
+      tags: tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    if(formError) return
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tags: tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    // redirect to home page
+    navigate("/");
   };
 
   return (
     <div className={styles.create_post}>
-      <h1>Criar Post</h1>
-      <p>Escreva sobre o que quiser e compartilhe </p>
-
+      <h2>Criar post</h2>
+      <p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
       <form onSubmit={handleSubmit}>
         <label>
-          <span>Título</span>
+          <span>Título:</span>
           <input
             type="text"
-            name="title"
+            name="text"
             required
-            placeholder="pense em um titulo"
+            placeholder="Pense num bom título..."
             onChange={(e) => setTitle(e.target.value)}
             value={title}
           />
         </label>
-
         <label>
-          <span>URL da imagem</span>
+          <span>URL da imagem:</span>
           <input
             type="text"
             name="image"
             required
-            placeholder="insira uma imagem que represente seu post"
+            placeholder="Insira uma imagem que representa seu post"
             onChange={(e) => setImage(e.target.value)}
             value={image}
           />
         </label>
-
         <label>
-          <span>Conteúdo</span>
+          <span>Conteúdo:</span>
           <textarea
             name="body"
             required
-            placeholder="insira o conteúdo do posto"
+            placeholder="Insira o conteúdo do post"
             onChange={(e) => setBody(e.target.value)}
             value={body}
-          />
+          ></textarea>
         </label>
-
         <label>
-          <span>Tags</span>
-          <textarea
-          type='text'
+          <span>Tags:</span>
+          <input
+            type="text"
             name="tags"
             required
-            placeholder="insira as tags separadas por virgula "
+            placeholder="Insira as tags separadas por vírgula"
             onChange={(e) => setTags(e.target.value)}
             value={tags}
           />
         </label>
-        <button type="submit" className="btn">Cadastrar</button>
-        {/* {!loading && <button type="submit" className="btn">Cadastrar</button>}
-        {loading && (
+        {!response.loading && <button className="btn">Criar post!</button>}
+        {response.loading && (
           <button className="btn" disabled>
-            Aguarde...
+            Aguarde.. .
           </button>
         )}
-
-        {error && <p className="error">{error}</p>} */}
+        {(response.error || formError) && (
+          <p className="error">{response.error || formError}</p>
+        )}
       </form>
     </div>
   );

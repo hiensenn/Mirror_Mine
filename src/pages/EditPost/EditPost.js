01dd1ebe67f562de2c2dbbf1ba5b7b57eb34c942
Tryvia Sua Frota Mais Inteligente <hiensen.cdl@gmail.com>
2024-11-28 18:12:11 -0300

    finishing CRUD update process

--- src/pages/EditPost/EditPost.js
+++ src/pages/EditPost/EditPost.js
@@ -1,10 +1,10 @@
 import styles from "./EditPost.module.css";
 
 import { useEffect, useState } from "react";
-import { useInsertDocument } from "../../hooks/useInsertDocument";
 import { useNavigate , useParams} from "react-router-dom";
 import { useAuthValue } from "../../context/AuthContext";
 import { useFetchDocument } from "../../hooks/useFetchDocument"; 
+import { useUpdateDocument } from "../../hooks/useUpdateDocument";
 import { Link } from "react-router-dom";
 
 const EditPost = () => {
@@ -34,7 +34,7 @@ const EditPost = () => {
 
   const navigate = useNavigate();
 
-  const { insertDocument, response } = useInsertDocument("posts");
+  const { updateDocument, response } = useUpdateDocument("posts");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -68,17 +68,19 @@ const EditPost = () => {
 
     if(formError) return
 
-    insertDocument({
-      title,
-      image,
-      body,
-      tags: tagsArray,
-      uid: user.uid,
-      createdBy: user.displayName,
-    });
+        const data = {
+            title,
+            image,
+            body,
+            tags: tagsArray,
+            uid: user.uid,
+            createdBy: user.displayName,
+        }
+        
+    updateDocument(id, data);
 
     // redirect to home page
-    navigate("/");
+    navigate("/dashboard");
   };
 
   return (

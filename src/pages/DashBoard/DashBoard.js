fb765116a0d940b43bb65cb9f838efb695bdec97
Tryvia Sua Frota Mais Inteligente <hiensen.cdl@gmail.com>
2024-11-28 16:20:44 -0300

    concluding dashboard structure

--- src/pages/DashBoard/DashBoard.js
+++ src/pages/DashBoard/DashBoard.js
@@ -15,6 +15,13 @@ const DashBoard = () => {
   //posts do usuario
   const {documents : posts, loading} = useFetchDocuments("posts", null, uid)
 
+  const deleteDocument = (id) => {
+
+    if(loading){
+      return <p>Carregando...</p>
+    }
+  }
+
   return (
     <div>
         <h1>DashBoard</h1>
@@ -23,12 +30,26 @@ const DashBoard = () => {
           <div className={styles.noposts}> <p>Você ainda não postou</p> <Link to='/posts/create' className='btn'>Criar um post</Link></div>
           
         ) : (
+          <>
           <div className={styles.posts}>
             <p>seus posts : </p>
+            
           </div>
+          {posts && posts.map((post) => <div key={post.id}>
+             <p>{post.title}</p>
+
+            <div>
+              <Link to={`/posts/${post.id}`}  className='btn btn-outline'>Ver </Link>
+              <Link to={`/posts/edit/${post.id}`}  className='btn btn-outline'>Editar </Link>
+              <button onClick={() => deleteDocument(post.id)} className='btn btn-outline'>Excluir</button>
+            </div>
+
+
+
+              </div>)}</>
         )}
 
-        {posts && posts.map((post) => <h2 key={post.id}>{post.title}</h2>)}
+        
     </div>
   )
 }
